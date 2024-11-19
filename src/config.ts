72117482306da2b84fbdbe2fd055e68fec6f1a66
Al Evan Castillo <45473100+Evanchii@users.noreply.github.com>
2024-11-19 22:18:08 +0800

    Initial Page

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export enum SOCIAL_LINKS {
+    FACEBOOK = "https://www.facebook.com/castillo00187/",
+    WEBSITE = "https://alevan.xyz/",
+    INSTAGRAM = "https://www.instagram.com/alevancastillo/"
+}
